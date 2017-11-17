b53d3f027505323044ff212768da107db5a26c01
dam1-mimanchadog <mimanchadog01@informatica.iesvalledeljerteplasencia.es>
2017-11-17 15:30:56 +0100

    lij

---
+++ Assets/Scripts/ControladorPersonaje.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPersonaje : MonoBehaviour {
+
+	public float fuerzaSalto = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
